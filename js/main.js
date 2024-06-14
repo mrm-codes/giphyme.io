7a789dbbd9897f87789e04f60da123d254ca45a5
Mario Matundulo <mrmatundulo@gmail.com>
2024-06-14 10:51:39 -0500

    finished setting up responsive navbar and added a clear function on the search properties

--- js/main.js
+++ js/main.js
@@ -8,46 +8,56 @@ Fetch three types of data:
 */
 //Responsive menu settings
 function menuBtn(){
-    var menu = document.getElementById("nav-list"); //element to be added the class
-    var navSection = document.getElementById("nav"); //navabar section
-    menu.classList.toggle("flex-menu"); //the class to add
-    navSection.classList.toggle("nav-section"); //the class to add
-    
-}
+    //responsive condition
+    var navSection = document.getElementById("nav"); //navabar 
+    var navGroup = document.getElementById("nav-group");
+    var btnActive = document.getElementById("btn-menu");
+    var responsiveClass = document.getElementById("nav-list");
 
-//search button
-
-async function sendRequest(){
-    var api_key = "api_key=b1zeTzlumEtPbUNcVYzQjeUi93hPyk1i";
-    var userInput = document.getElementById("search").value;
-    const resultBox = document.querySelector(".keyWordList");
-    //const inputBox =document.getElementById("search")
-
-   await fetch(`https://api.giphy.com/v1/gifs/search?${api_key}&q=${userInput}&limit=25&offset=0&rating=g&lang=en&bundle=clips_grid_picker`)
-   .then(function(data){
-    return data.json()
-   })
-   .then(function(json){
-   
-   for (let i = 0; i<30; i++){
-    //urlPaths = console.log(json.data[i].images.fixed_width.url) 
-    //var keyWordList = [json.data.title]
-    //console.log(json.data[i])
-    var imgPath = json.data[i].images.fixed_width.url
-    var img = document.createElement("img")
-    img.setAttribute("src", imgPath)
-    document.getElementById("jsSrcResults").appendChild(img)
-    //console.log(keyWordList)
-    
+    if(responsiveClass.className === "navbar"){
+        responsiveClass.className += "responsive-menu";
+    } else {
+        responsiveClass.className = "navbar";
     }
 
-   })
-
+    navSection.classList.toggle("nav-section"); //the class to add
+    navGroup.classList.toggle("active");
+    btnActive.classList.toggle("btn-active")
     
 }
 
+//search section
+async function sendRequest(){
+    //clear the previous result
+    clearResults();
 
+    //delcarations
+    var api_key = "api_key=b1zeTzlumEtPbUNcVYzQjeUi93hPyk1i";
+    var userInput = document.getElementById("search").value;
 
+    await fetch(`https://api.giphy.com/v1/gifs/search?${api_key}&q=${userInput}&limit=25&offset=0&rating=g&lang=en&bundle=clips_grid_picker`)
+    .then(function(data){
+        return data.json()
+    })
+    .then(function(json){
+    
+    //getting more than a single result
+    for (let i = 0; i<30; i++){
+        var imgPath = json.data[i].images.fixed_width.url
+        var img = document.createElement("img")
+
+        //creating images attributes
+        img.setAttribute("src", imgPath)
+        document.getElementById("jsSrcResults").appendChild(img)
+    
+    }
+   })  
+
+   //clear result
+   function clearResults (){
+    document.getElementById("jsSrcResults").innerHTML = '';
+   } 
+};
 
 
 
